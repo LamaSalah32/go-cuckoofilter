989917dfe4fb9d5337d7f2bd52175f04c30d084c
lamasalah32 <lamasalah32@gmail.com>
2025-09-15 14:17:46 +0300

    add MinFingerprintBits function to fingerprint file

--- fingerprint.go
+++ fingerprint.go
@@ -1,12 +1,14 @@
 package cuckoofilter
 
 import (
+	"math"
 	"github.com/cespare/xxhash/v2"
 )
 
 func fprint(item []byte, fpSize uint) uint64 {
 	h := xxhash.Sum64(item)
-	if fpSize > 0 || fpSize < 64 {
+	
+	if fpSize > 0 && fpSize < 64 {
 		mask := uint64(1<<fpSize) - 1
 		h &= mask
 	} 
@@ -14,9 +16,18 @@ func fprint(item []byte, fpSize uint) uint64 {
 	if h == 0 {
 		h = 1
 	}
+	
 	return h
 }
 
 func hash (item []byte)  uint64 {
 	return  xxhash.Sum64(item)
+}
+
+func MinFingerprintBits(n uint64, b uint) uint {
+	if n == 0 || b == 0 {
+		return 0
+	}
+
+	return uint(math.Max(4, math.Ceil(math.Log2(float64(n))/float64(b))))
 }
